ab94c19a83e9963c1d73323ee96f7c7ee4967126
Acring <392398434@qq.com>
2017-07-29 17:08:50 +0800

    添加指定语言的查询和命令行检查

--- README.md
+++ README.md
@@ -6,4 +6,27 @@
 
 > git clone git@github.com:Acring/acfy.git
 
+## Help
 
+>              acfy
+命令行翻译脚本 使用有道翻译API
+
+不指定语言的查询
+例 acfy i am your father
+指定语言查询
+例 acfy -l zh "你好 朋友" ja
+
+Options:
+    -l      指定语言的查询
+    -h      显示帮助文档
+
+注意在指定语言查询时，长句子要用单引号或双引号引起来
+语言转换表
+    zh          中文
+    ja          日文
+    en          英文
+    ko          韩语
+    fr          法语
+    ru          俄语
+    pt          葡萄牙
+    es          西班牙
--- acfy.py
+++ acfy.py
@@ -1,11 +1,34 @@
-# python3
+#!python3
 # coding=utf-8
+"""
+             acfy
+命令行翻译脚本 使用有道翻译API
 
+不指定语言的查询
+例 acfy i am your father
+指定语言查询
+例 acfy -l zh "你好 朋友" ja
+
+Options:
+    -l      指定语言的查询
+    -h      显示帮助文档
+
+注意在指定语言查询时，长句子要用单引号或双引号引起来
+语言转换表
+    zh          中文
+    ja          日文
+    en          英文
+    ko          韩语
+    fr          法语
+    ru          俄语
+    pt          葡萄牙
+    es          西班牙
+"""
 import requests
 import hashlib
 import random
 import sys
-
+import getopt
 
 https_url = "https://openapi.youdao.com/api"  # Api的https地址
 app_key = "223abc56842b3d2d"  # 应用ID
@@ -23,11 +46,17 @@ language = {  # 语言转换表
 }
 
 
-# 生成md5sign
+# 生成md5签名
 def to_sign(q, salt):
+    """
+    签名要进行UTF-8编码(否则中文无法翻译)
+    :param q: 翻译文本
+    :param salt: 随机数
+    :return: sign: md5签名
+    """
     sign = app_key + q + salt + secret_key
     m = hashlib.md5()
-    m.update(sign.encode('gb2312'))
+    m.update(sign.encode('utf-8'))
     sign = m.hexdigest()
     return sign
 
@@ -40,14 +69,20 @@ def get_api_url(sign, salt, q="Hello World", from_lan="auto", to_lan="auto"):
 
 # 解析返回的json字段生成翻译
 def resolve_res(trans_json):
+    """
+    把API返回的json字段解析成文字
+    :param trans_json: json
+    :return: string
+    """
     if type(trans_json) != dict:
         print("返回字段格式不正确:",type(trans_json))
         return ""
     if trans_json["errorCode"] != '0':  # 查询正确与否
-        print("查询失败,errorCode:{},可查询官方文档:{}".format(trans_json.errorCode, api_docs))
+        print("查询失败,errorCode:{},可查询官方文档:{}".format(trans_json["errorCode"], api_docs))
         return ""
 
-    result = "==============翻译结果==============\n"  # 翻译结果
+    result = "==============acfy翻译结果==============\n"  # 翻译结果
+
     if 'l' in trans_json:  # 转换关系
         lan2lan = trans_json['l'].split('2')
         result += "{}转{}".format(language[lan2lan[0]], language[lan2lan[1]]) + '\n'
@@ -74,12 +109,56 @@ def resolve_res(trans_json):
 
     return result
 
-if __name__ == '__main__':
-    q = " ".join(sys.argv[1:])  # 获取要翻译的文本
-    q="hi"  # TEST
+
+def resolve_opts(argv):
+    """
+    解析命令行参数
+    :param argv: 命令行参数
+    1.不指定语言的查询(中英互译) 如'hello'
+    2.指定语言的查询 如'-l zh 你好 ja'
+    :return: query:dict 如{'form':'zh', 'to': 'ja', 'q': '你好'}
+    """
+    query = {'form': 'auto', 'to': 'auto', 'q': ''}
+    short_opts = 'hl'
+    lan = ['zh', 'ja', 'en', 'ko', 'fr', 'ru', 'pt', 'es']
+    opts, argvs = getopt.getopt(argv[1:], short_opts)
+    for o, a in opts:
+        if o == '-h':  # 显示帮助文档
+            print(__doc__)
+            sys.exit(0)
+        if o == '-l':  # 指定语言的翻译
+            if argvs[0] in lan and argvs[2] in lan:
+                query['form'] = argvs[0]
+                query['q'] = argvs[1]
+                query['to'] = argvs[2]
+                break
+            else:
+                print("指定语言不正确")
+                sys.exit(1)
+    else:  # 不指定语言的查询
+        q = " ".join(argvs)
+        query['q'] = q
+    return query
+
+
+def main():
+    argv = sys.argv
+
+    query = resolve_opts(argv)
+
+    if query['q'] == '':
+        print("翻译文本为空,请重新输入")
+        return 1
     salt = str(random.randint(12345, 56789))  # 生成随机数
-    sign = to_sign(q, salt)  # 生成签名
-    api_url = get_api_url(q=q, sign=sign, salt=salt)  # TODO 翻译类型
+    sign = to_sign(query['q'], salt)  # 生成签名
+    api_url = get_api_url(q=query['q'], sign=sign, salt=salt, from_lan=query['form'], to_lan=query['to'],)
     translation_json = requests.get(api_url)
+    if not translation_json.ok:
+        print("网络错误:{}".format(translation_json))
+        return 2
     result = resolve_res(trans_json=translation_json.json())
     print(result)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
